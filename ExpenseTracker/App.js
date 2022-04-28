0e81d4fbd1cbd3dc6c31969f8c953bf6290f1ed0
gurjindersingh <gurjindersingh663@gmail.com>
2022-04-28 22:17:10 +0530

    initial commit - project created

---
+++ ExpenseTracker/App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+
+function App() {
+  return <Text>Expense Tracker</Text>
+}
+
+export default App;

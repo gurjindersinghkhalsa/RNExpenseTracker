92dc18732582bea4fe1601511077d658e8119b5f
gurjindersingh <gurjindersingh663@gmail.com>
2022-04-30 21:16:16 +0530

    Expense output components added

---
+++ ExpenseTracker/Components/ExpensesOutput/ExpesesList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {FlatList, View} from 'react-native';
+
+function ExpensesList() {
+  return (
+    <View>
+      <FlatList/>
+    </View>
+  );
+}
+
+export default ExpensesList;
---
+++ ExpenseTracker/constants/style.js
@@ -0,0 +1,5 @@
+export default GlobalStyle = {
+    colors: {
+        blue: 'blue'
+    }
+}

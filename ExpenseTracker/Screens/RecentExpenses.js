0b10d1f33758a52b9b3712b333f719d12fdd0759
gurjindersingh <gurjindersingh663@gmail.com>
2022-05-06 11:40:54 +0530

    https file added and post request made

--- ExpenseTracker/Screens/RecentExpenses.js
+++ ExpenseTracker/Screens/RecentExpenses.js
@@ -1,12 +1,23 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
+import { useState } from 'react/cjs/react.production.min';
 
 import ExpensesOutput from '../Components/ExpensesOutput/ExpensesOutput';
 import {ExpenseContext} from '../store/expenses-context';
 import {getDateMinusDays} from '../util/Date';
+import { fetchExpenses } from '../util/https';
 
 function RecentExpense() {
-  const expenseCntx = useContext(ExpenseContext);
-  const recentExpenses = expenseCntx.expenses.filter(function (expense) {
+  // const expenseCntx = useContext(ExpenseContext);
+  const [fetchedExpenses, setFetchedExpense] = useState([]);
+  useEffect(() => {
+    async function getExpenses() {
+      const expense = await fetchExpenses();
+      setFetchedExpense(expense);
+    }
+    getExpenses();
+  },[])
+ // const recentExpenses = expenseCntx.expenses.filter(function (expense) {
+  const recentExpenses = fetchedExpenses.expenses.filter(function (expense) {
     const today = new Date();
     const date7daysAgo = getDateMinusDays(today, 7);
     return expense.date >= date7daysAgo && expense.date <= today;
